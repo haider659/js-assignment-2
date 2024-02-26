5a13b1db379d1e65b86ff573cff88d1789767d71
haider659 <haderkhan659@gmail.com>
2024-02-26 21:24:48 +0500

    js 2

---
+++ app.js
@@ -0,0 +1,102 @@
+// chapter 6
+// Q1
+x++;
+// this is llegal Expression
+// Q2
+var num=100;
+console.log(--num);
+// Q3
+var x=50;
+var y=x++;
+console.log(y);
+// ans is 50
+// Q4
+var y=50;
+var z=y--;
+console.log(z);
+// ans is 50
+// Q5
+var newNum = num = num - 1;
+console.log(newNum);
+// Q6
+ var num = 5;
+ var newNum = ++num; 
+ console.log(newNum);
+//  Q7
+var num =20;
+num++;
+alert("the new num is:" + num);
+
+
+
+// // chapter 7
+// // Q1
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum);
+// // Q2
+var calculatedNum = (2 + 2) * 6;
+console.log(calculatedNum);
+// // Q3
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log(calculatedNum);
+// // Q4
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log(calculatedNum);
+// // Q5
+var calculatedNum = (2 + 2) * (4 + 10);
+console.log(calculatedNum);
+// // Q6
+var calculatedNum = 2 + 2 * 4 + 10;
+console.log(calculatedNum);
+// // Q7
+var calculatedNum = 4 / 2 * 4;
+console.log(calculatedNum);
+// // producing 5 is not possible
+
+// // chapter 8
+// // Q1
+var num = "2" + "2";
+console.log(num);
+// // Q2
+var message = ("Hello " + "Dolly");
+console.log(message);
+// // Q3
+alert("33" + 3);
+// // Q4
+alert("Pakistan  " + "Zindabad");
+// // Q5
+var Name = "haider";
+var num = "55"
+alert(Name + num);
+// // Q6
+var num = "10";
+var num2 = "20";
+var result = (num + num2);
+console.log(result);
+// // chapter 9
+// // Q1
+var Name = prompt("Enter your name");
+document.write(Name);
+// // Q2
+var country = prompt("Country?", "China");
+document.write(country);
+// // Q3
+var yourName = prompt("Enter Your Name");
+// // Q4
+var userName = prompt(" enter your name:", "Haider khan");
+document.write(userName);
+// // Q5
+var message = "Hello"
+var defaultResponse = "iam haider";
+var userResponse = prompt(message , defaultResponse);
+// // Q6
+var message = "Hello"
+var defaultResponse = "iam haider";
+var userResponse = prompt(message , defaultResponse);
+alert(userResponse);
+
+
+
+
+
+
